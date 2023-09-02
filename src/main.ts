f4bdcc2280c980cfdf6e45dccfaf0b3467d74fe0
fanchaowei <597064177@qq.com>
2023-09-02 22:22:15 +0800

    feat: 引用 router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/styles/scss/index.scss'
+import { setupRouter } from '@/router'
 
 const app = createApp(App)
+setupRouter(app)
 
 app.mount('#app')
