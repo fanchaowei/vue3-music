92cf6314cfeb9fdeb3f2979659f2d17c23a5830a
fanchaowei <597064177@qq.com>
2023-09-02 22:22:03 +0800

    fix: 删除 html 样式

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,13 @@ export default defineComponent({
 </script>
 <script setup lang="ts">
 import HeaderLayout from '@/components/layout/header/header.vue'
+import TabLayout from '@/components/layout/tab/tab.vue'
 </script>
 
 <template>
   <HeaderLayout></HeaderLayout>
+  <TabLayout></TabLayout>
+  <router-view></router-view>
 </template>
 
-<style scoped>
-html {
-  font-size: 10px;
-}
-</style>
+<style scoped></style>
