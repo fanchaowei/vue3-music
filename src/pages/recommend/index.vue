b6cddc62933f563298a8fa113faa957a90a33905
fanchaowei <597064177@qq.com>
2023-09-26 19:42:40 +0800

    feat: 实现轮播图和滚动组件的使用

--- src/pages/recommend/index.vue
+++ src/pages/recommend/index.vue
@@ -1,20 +1,117 @@
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { fetchGetRecommend } from '@/service'
+import { ISlider, IRecommend, IAlbum } from '@/types'
+import Slider from '@/components/base/slider/slider.vue'
+import Scroll from '@/components/base/scroll/scroll.vue'
 export default defineComponent({
   name: 'RecommendPage',
 })
 </script>
 
 <script setup lang="ts">
-onMounted(async () => {
-  const res = await fetchGetRecommend()
-  console.log(res)
-})
+const sliders = ref<ISlider[]>([])
+const albums = ref<IAlbum[]>([])
+
+const res: IRecommend = await fetchGetRecommend()
+sliders.value = res.sliders
+albums.value = res.albums
 </script>
 
 <template>
-  <div></div>
+  <div class="recommend">
+    <Scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <Slider v-if="sliders.length" :sliders="sliders" />
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in albums" :key="item.id" class="item">
+              <div class="icon">
+                <!-- v-lazy 是图片懒加载插件给予的 -->
+                <img width="60" height="60" :src="item.pic" />
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Scroll>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  overflow: scroll;
+  .recommend-content {
+    height: 100%;
+    overflow: hidden;
+    .slider-wrapper {
+      position: relative;
+      width: 100%;
+      height: 0;
+      padding-top: 40%;
+      overflow: hidden;
+      .slider-content {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
+
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+        }
+        .name {
+          margin-bottom: 10px;
+          color: $color-text;
+        }
+        .title {
+          color: $color-text-d;
+        }
+      }
+    }
+  }
+}
+</style>
