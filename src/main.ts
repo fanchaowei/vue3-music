c200aaf938f118284ad401c3ecb72fec52172ec2
fanchaowei <597064177@qq.com>
2023-10-08 22:10:44 +0800

    feat: 注册自定义指令和注册插件

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/styles/scss/index.scss'
 import { setupRouter } from '@/router'
+import { setupDirective } from '@/directives'
+import { setupPlugins } from '@/plugins'
 
 const app = createApp(App)
 setupRouter(app)
+// 注册自定义指令
+setupDirective(app)
+// 初始化配置
+setupPlugins(app)
 
 app.mount('#app')
