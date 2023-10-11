e0b36fe0bc2f84592f1c506a2fc99c62d97ebde6
fanchaowei <597064177@qq.com>
2023-10-11 21:27:15 +0800

    feat: 初步完成 singer 列表展示

--- src/pages/singer/index.vue
+++ src/pages/singer/index.vue
@@ -1,14 +1,34 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { fetchGetSingers } from '@/service'
+import { ISingerColumn } from '@/types'
+import IndexList from '@/components/base/indexList/indexList.vue'
+
 export default defineComponent({
   name: 'SingerPage',
 })
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const singersData = ref<ISingerColumn[]>([])
+
+onMounted(async () => {
+  const res = await fetchGetSingers()
+  singersData.value = res.singers
+})
+</script>
 
 <template>
-  <div>singer</div>
+  <div v-loading="!singersData.length" class="singer">
+    <IndexList :singers-data="singersData" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
