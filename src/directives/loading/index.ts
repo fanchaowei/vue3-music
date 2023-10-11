aec4d920ebc2bbb9a3c9618a5e4cee8b06129dbb
fanchaowei <597064177@qq.com>
2023-10-11 19:16:27 +0800

    feat: 优化  loading 自定义指令

--- src/directives/loading/index.ts
+++ src/directives/loading/index.ts
@@ -1,15 +1,24 @@
 import { createApp } from 'vue'
 import type { Directive, DirectiveHook, ComponentPublicInstance, DirectiveBinding, App } from 'vue'
 import Loading from './loading.vue'
+import { addClass, removeClass } from '@/composables'
 
 interface HTMLElementEx extends HTMLElement {
   instance?: ComponentPublicInstance
 }
+interface LoadingInstance extends ComponentPublicInstance {
+  setTitle: (title: string) => void
+}
 
 const mounted: DirectiveHook = (el: HTMLElementEx, binding: DirectiveBinding) => {
   const app = createApp(Loading)
-  const instance = app.mount(document.createElement('div'))
+  const instance = app.mount(document.createElement('div')) as LoadingInstance
   el.instance = instance
+
+  const title = binding.arg
+  if (title && typeof title === 'string') {
+    instance.setTitle(title)
+  }
 
   if (binding.value) {
     append(el)
@@ -22,10 +31,18 @@ const updated: DirectiveHook = (el: HTMLElementEx, binding: DirectiveBinding) =>
   }
 }
 
-const append = (el: HTMLElementEx) => {
+const positionClassName = 'g-relative'
+function append(el: HTMLElementEx) {
+  const positionText = ['relative', 'absolute', 'fixed']
+  const style = getComputedStyle(el)
+  // 父元素的 position 如果没有设置，则给父元素添加 relative
+  if (positionText.includes(style.position)) {
+    addClass(el, positionClassName)
+  }
   el.appendChild(el.instance?.$el)
 }
 function remove(el: HTMLElementEx) {
+  removeClass(el, positionClassName)
   el.removeChild(el.instance?.$el)
 }
 
